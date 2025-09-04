42a0c656113483e193caf5218dd1eb8a35eb62fc
gunelbalayeva <gunelbalayeva97@gmail.com>
2025-09-04 18:46:23 +0400

    Initial Commit

---
+++ ToDoApp_SwiftUi/ToDoApp_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp_SwiftUiApp.swift
+//  ToDoApp_SwiftUi
+//
+//  Created by User on 04.09.25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
